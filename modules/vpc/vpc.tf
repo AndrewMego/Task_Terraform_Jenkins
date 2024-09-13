e33a7c2362479ed30a0a85d1e433b44246960af5
AndrewMego <36993680+AndrewMego@users.noreply.github.com>
2024-09-13 20:27:20 +0300

    Update vpc.tf

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -1,1 +1,18 @@
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+}
 
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.subnet_cidrs[count.index]
+  map_public_ip_on_launch = true
+}
+
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "public_subnets" {
+  value = aws_subnet.public[*].id
+}
